b18013389f11a37fa2faf46cc0e16e1bb9fe5345
Brian <brianj.pajak@gmail.com>
2020-10-02 19:16:43 -0400

    Initial commit

---
+++ Assets/_Scripts/PlayerShooting.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    const string verticalFire = "VerticalFire";
+    const string horizontalFire = "HorizontalFire";
+    [SerializeField] GameObject bullet;
+    Transform parent;
+    Vector3 aiming;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        parent = this.transform.parent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        aiming = new Vector3(Input.GetAxis(  horizontalFire), Input.GetAxis(  verticalFire), 0);
+        transform.rotation = new Quaternion(aiming);
+        
+    }
+}
