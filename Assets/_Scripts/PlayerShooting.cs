314062fd5a041286e0fb023de8d499a369d738ac
Brian <brianj.pajak@gmail.com>
2020-10-02 20:16:49 -0400

    basic shooting mechanics

--- Assets/_Scripts/PlayerShooting.cs
+++ Assets/_Scripts/PlayerShooting.cs
@@ -1,26 +1,48 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions.Comparers;
 
 public class PlayerShooting : MonoBehaviour
 {
-    const string verticalFire = "VerticalFire";
-    const string horizontalFire = "HorizontalFire";
-    [SerializeField] GameObject bullet;
-    Transform parent;
-    Vector3 aiming;
-    
-    // Start is called before the first frame update
+    [SerializeField] Texture2D cursor;
+    [SerializeField] Transform gun;
+    [SerializeField] Transform barrel;
+    [SerializeField] Transform bulletPrefab;
+    float fireDelay = .5f;
+    float nextFireTime = 0;
+    float fireOffset = .5f;
+
+
+    // Use this for initialization
     void Start()
     {
-        parent = this.transform.parent;
+        Cursor.SetCursor(cursor, Vector2.zero, CursorMode.Auto);
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-        aiming = new Vector3(Input.GetAxis(  horizontalFire), Input.GetAxis(  verticalFire), 0);
-        transform.rotation = new Quaternion(aiming);
-        
+        Aiming();
+        if (Input.GetMouseButtonDown(0)&& CanFire())
+        {
+            nextFireTime = Time.time + fireDelay;
+            Instantiate(bulletPrefab, barrel.transform.position, gun.transform.rotation);
+        }
+    }
+
+    private bool CanFire()
+    {
+        return Time.time >= nextFireTime;
+    }
+
+    private void Aiming()
+    {
+        Vector2 dir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        gun.rotation = rotation;
     }
 }
