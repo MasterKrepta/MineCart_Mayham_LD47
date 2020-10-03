ff84f9cd255fafe2ee7215517598366c733257b3
Brian <brianj.pajak@gmail.com>
2020-10-03 18:53:22 -0400

    Fix bug with UI linking on  junctions

---
+++ Assets/_Scripts/Bat.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bat : MonoBehaviour
+{
+    [SerializeField] float fireRate, range, movespeed;
+    [SerializeField] Transform spitPrefab, barrel;
+    
+    Player _player;
+    float dist;
+    float nextfire;
+    bool CanFire = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = FindObjectOfType<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dist = Vector3.Distance(_player.transform.position, transform.position);
+
+        var dir = _player.transform.position - transform.position;
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        transform.position = Vector2.MoveTowards(transform.position, _player.transform.position, movespeed * Time.deltaTime);
+
+
+        CanFire = Canfire();
+        if (dist <= range && CanFire)
+        {
+            nextfire = Time.time + fireRate;
+            Instantiate(spitPrefab, barrel.position, barrel.rotation);
+        }
+    }
+
+    bool Canfire()
+    {
+        return Time.time >= nextfire;
+    }
+}
--- Assets/_Scripts/TrackController.cs
+++ Assets/_Scripts/TrackController.cs
@@ -6,7 +6,9 @@ using UnityEngine;
 public class TrackController : MonoBehaviour
 {
     [SerializeField] List<TrackPoint> currentNodes;
-    public List<TrackPoint> Track1, Track2, Track3, Track4, Track5;
+    public List<TrackPoint> Track1, Track2, Track3, Track4, Track5, Track6, Track7, Track8, Track9, Track10,
+                            Track11, Track12, Track13, Track14, Track15,Track16,Track17,Track18,Track19,Track20,
+                            Track21, Track22, Track23,Track24;
     public int currentPosition;
     TrackPoint closestNode;
     // Start is called before the first frame update
@@ -60,6 +62,63 @@ public class TrackController : MonoBehaviour
                 break;
             case "5":
                 newTrack = Track5;
+                break;
+            case "6":
+                newTrack = Track6;
+                break;
+            case "7":
+                newTrack = Track7;
+                break;
+            case "8":
+                newTrack = Track8;
+                break;
+            case "9":
+                newTrack = Track9;
+                break;
+            case "10":
+                newTrack = Track10;
+                break;
+            case "11":
+                newTrack = Track11;
+                break;
+            case "12":
+                newTrack = Track12;
+                break;
+            case "13":
+                newTrack = Track13;
+                break;
+            case "14":
+                newTrack = Track14;
+                break;
+            case "15":
+                newTrack = Track15;
+                break;
+            case "16":
+                newTrack = Track16;
+                break;
+            case "17":
+                newTrack = Track17;
+                break;
+            case "18":
+                newTrack = Track18;
+                break;
+            case "19":
+                newTrack = Track19;
+                break;
+            case "20":
+                newTrack = Track20;
+                break;
+            case "21":
+                newTrack = Track21;
+                break;
+            case "22":
+                newTrack = Track22;
+                break;
+            case "23":
+                newTrack = Track23;
+                break;
+            case "24":
+                newTrack = Track24;
                 break;
             default:
                 break;
